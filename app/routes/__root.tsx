e09a25021b415074c5606c5678b887fba4f46707
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-02-11 19:26:56 +0900

    feat: add tailwind

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -3,42 +3,50 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Meta, Scripts } from '@tanstack/start'
 import type { ReactNode } from 'react'
 
+import appCss from "@/styles/app.css?url"
+
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-  }),
-  component: RootComponent,
+    head: () => ({
+        meta: [
+            {
+                charSet: 'utf-8',
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                title: 'TanStack Start Starter',
+            },
+        ],
+        links: [
+            {
+                rel: "stylesheet",
+                href: appCss,
+            },
+        ],
+    }),
+    component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+    return (
+        <RootDocument>
+            <Outlet />
+        </RootDocument>
+    )
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html>
-      <head>
-        <Meta />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+    return (
+        <html>
+            <head>
+                <Meta />
+            </head>
+            <body>
+                {children}
+                <Scripts />
+            </body>
+        </html>
+    )
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -6,6 +7,9 @@ export const Route = createFileRoute('/')({
 
 function Home() {
     return (
-        <h1>Hello World</h1>
+        <>
+            <h1>Hello World</h1>
+            <Button>Click me</Button>
+        </>
     )
 }
