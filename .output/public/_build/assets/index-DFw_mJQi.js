380ba9cfe8dd4d5cbd96efe1ff96eb9dbb2684c9
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-02-17 20:23:51 +0900

    Delete .output directory

--- .output/public/_build/assets/index-DFw_mJQi.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./client-BDQX6FdU.js";const e=function(){return n.jsx(n.Fragment,{children:n.jsx("h1",{children:"WTDT"})})};export{e as component};
--- .output/server/chunks/build/index-ldWUXKM6.mjs
+++
@@ -1,8 +0,0 @@
-import { jsx, Fragment } from 'react/jsx-runtime';
-
-const r = function() {
-  return jsx(Fragment, { children: jsx("h1", { children: "WTDT" }) });
-};
-
-export { r as component };
-//# sourceMappingURL=index-ldWUXKM6.mjs.map
