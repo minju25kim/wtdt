e0fd78690d9cfcd1529ddae1d4fb8ad26e8d82ad
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-02-18 09:50:50 +0900

    ci: check baseUrl

--- app/routes/api/auth/github.tsx
+++ app/routes/api/auth/github.tsx
@@ -4,10 +4,7 @@ import { getSupabaseServerClient } from '@/utils/supabase'
 export const APIRoute = createAPIFileRoute('/api/auth/github')({
   GET: async () => {
     const supabase = await getSupabaseServerClient()
-    const baseUrl =
-      process.env.NODE_ENV === 'production'
-        ? process.env.SITE_URL
-        : 'http://localhost:3000'
+    const baseUrl = process.env.SITE_URL || 'http://localhost:3000'
 
     const { data } = await supabase.auth.signInWithOAuth({
       provider: 'github',
--- app/routes/api/auth/google.tsx
+++ app/routes/api/auth/google.tsx
@@ -4,11 +4,8 @@ import { getSupabaseServerClient } from '@/utils/supabase'
 export const APIRoute = createAPIFileRoute('/api/auth/google')({
   GET: async () => {
     const supabase = await getSupabaseServerClient()
-    const baseUrl =
-      process.env.NODE_ENV === 'production'
-        ? process.env.SITE_URL
-        : 'http://localhost:3000'
-
+    const baseUrl = process.env.SITE_URL || 'http://localhost:3000'
+    
     const { data } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
